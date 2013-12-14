55e165cd278b4390736236a61715a5662bf05c1f
Raikkonen Lee <xyz7689@hotmail.com>
2013-12-14 16:30:11 +0800

    Classes and Constructors

---
+++ Chapter9/9-2.js
@@ -0,0 +1,23 @@
+/*Range物件要使用New的方式調用prototype才會正確的執行*/
+function test(){
+  var r = new Range(1, 3);
+  alert(r.includes(2));
+  var a = r.foreach();
+  alert(a);
+}
+/*定義建構式的Function*/
+function Range(from, to){
+  this.from = from;
+  this.to = to;
+}
+
+/*定義Range的prototype當作建構式*/
+Range.prototype = {
+  includes: function(x){ return this.from <= x && x <= this.to;},
+  foreach: function(f) {
+    var returnResult = "";
+    for (var x = Math.ceil(this.from); x <= this.to; x++) returnResult += x.toString();
+    return returnResult;
+  },
+  toString: function() { return "(" + this.from + "..." + this.to + ")"; }
+};
